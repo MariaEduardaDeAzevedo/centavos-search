0b2344ba3b0f3ab2fcd09662a752f882ebecf5c1
Beatriz Miranda <beatriza.dmiranda@gmail.com>
2021-09-16 19:40:20 -0300

    Arredondamento da porcentagem dos aprovados
    Coloquei  na parte de analytics e com duas casas decimais no arredondamento  :)

--- backend/functions.py
+++ backend/functions.py
@@ -146,9 +146,7 @@ def analytics():
 
     obj_analytics['max'] = max(list_median)
     obj_analytics['min'] = min(list_median)
-
     obj_analytics['mean'] = round(sum(list_median)/len(list_median))
-
-    obj_analytics['percent_approved'] = obj_analytics['approved']/len(list_median)
+    obj_analytics['percent_approved'] = round(obj_analytics['approved']/len(list_median), 2)
 
     return obj_analytics
